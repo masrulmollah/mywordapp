a489b0167767e6923bc8d0de8c05410a0d3c6226
masrulmollah <54163010+masrulmollah@users.noreply.github.com>
2024-08-26 22:02:44 +0600

    Update main2.py

--- main2.py
+++ main2.py
@@ -44,7 +44,7 @@ def main():
                     class_data = st.session_state.word_data[st.session_state.word_data['Class'] == class_name]
 
                     for word in class_data['Word']:
-                        col1, col2 = st.columns([2, 3])
+                        col1, col2 = st.columns([1, 3])
                         col1.write(word)
                         if col2.button("X", key=f"delete_{word}"):
                             st.session_state.word_data = delete_word(st.session_state.word_data, word)
